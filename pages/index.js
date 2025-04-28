86f2d4f23bf90bb8c1cccca6e68cbda6811c8b3e
Skrrrrlabs <skrrrrlabs@gmail.com>
2025-04-28 18:35:30 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,22 @@ const Home = () => {
         {/* Binance Card */}
         <div style={cardStyle}>
           <img src="/binance.png" alt="Binance" style={logoStyle('#F0B90B')} />
-          <p>바이낸스 현물/선물 10% 수수료 할인!</p>
+          <p style={textStyle}>바이낸스 현물/선물 10% 수수료 할인!</p>
           <a href="https://accounts.binance.com/register?ref=V5EBF1SH" target="_blank" rel="noopener noreferrer" style={binanceBtn}>가입하기</a>
         </div>
 
         {/* OKX Card */}
         <div style={cardStyle}>
           <img src="/okx.png" alt="OKX" style={logoStyle('#000')} />
-          <p>OKX 평생 수수료 할인 + 20% 페이백!</p>
+          <p style={textStyle}>OKX 평생 수수료 할인 + 20% 페이백!</p>
           <a href="https://www.okx.com/join/SKRRRR" target="_blank" rel="noopener noreferrer" style={okxBtn}>가입하기</a>
         </div>
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '40px', marginTop: '50px' }}>
-        {/* Add other Cards */}
+
+        {/* Bitget Card */}
         <div style={cardStyle}>
           <img src="/bitget.png" alt="Bitget" style={logoStyle('#1c72c6')} />
-          <p>비트겟 평생 수수료 할인 + 20% 페이백!</p>
+          <p style={textStyle}>비트겟 평생 수수료 할인 + 20% 페이백!</p>
           <a href="https://www.bitget.com/register?ref=SKRRRR" target="_blank" rel="noopener noreferrer" style={okxBtn}>가입하기</a>
-        </div>
-        <div style={cardStyle}>
-          <img src="/bingx.png" alt="BingX" style={logoStyle('#00a9e0')} />
-          <p>BingX 평생 수수료 할인 + 45% 페이백!</p>
-          <a href="https://www.bingx.com/register?ref=SKRRRR" target="_blank" rel="noopener noreferrer" style={okxBtn}>가입하기</a>
-        </div>
-        <div style={cardStyle}>
-          <img src="/bybit.png" alt="Bybit" style={logoStyle('#4f7db8')} />
-          <p>바이비트 수수료 할인 + 20%</p>
-          <a href="https://www.bybit.com/register?ref=SKRRRR" target="_blank" rel="noopener noreferrer" style={okxBtn}>가입하기</a>
         </div>
       </div>
     </div>
@@ -46,8 +35,8 @@ const cardStyle = {
   border: '1px solid #eee',
   borderRadius: '12px',
   width: '250px',
-  height: '350px', // 카드를 일정 높이로 설정
-  padding: '30px',
+  height: '280px', // 카드 높이 줄임
+  padding: '20px',
   boxShadow: '0 4px 12px rgba(0,0,0,0.1)',
   backgroundColor: '#fff',
   textAlign: 'center',
@@ -87,5 +76,9 @@ const logoStyle = (color) => ({
   borderColor: color,
 });
 
-export default Home;
+const textStyle = {
+  fontSize: '16px',  // 폰트 크기를 한 치수 내려서 글씨가 두번째 줄로 넘어가지 않게 함
+  lineHeight: '1.4',  // 줄 간격 조금 더 넓힘
+};
 
+export default Home;
