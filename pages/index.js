27daf093dbf7412de024dd870a7a39f7d0c5bb8c
Skrrrrlabs <skrrrrlabs@gmail.com>
2025-04-28 14:25:56 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 Welcome to CRYPTO LABS 🚀</h1>
+      <p>오빠의 공식 레퍼럴 사이트가 오픈했습니다!</p>
+    </div>
+  )
+}
